cf7be68c37b6e7187468bbb6d0395edb373f17b5
ANdrewDUh <andrewngo0609@gmail.com>
2025-08-24 17:54:24 +0700

    SE01: First Buid Class Student

---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student  {
+	private String name;
+	private String id;
+	private int credits;
+	private double points;
+	
+	public static void main(String[] args) {
+		System.out.println("1 2 buckle my shoe");
+	}
+	
+}
